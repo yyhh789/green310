f3526ff2a5e6181b1f61db42c2d1c8602e5f566a
yyhh789 <129481978+yyhh789@users.noreply.github.com>
2023-07-07 16:54:00 +0900

    230707

--- project003/js/ex002.js
+++ project003/js/ex002.js
@@ -1,53 +1,41 @@
 $(document).ready(function(){
-    // 마우스가 움직이는 값을 찾아라
-
+    // 마우스가 움직이는 값을 찾아라 
+    
     $(window).mousemove(function(e){
-
-        let x = e.pageX;
+    
+        let x= e.pageX;
         let y = e.pageY;
-
-        $('.cursor').css({'left':'x' ,'top':'y'});
-
-        
-
+    
+        $('.cursor').css({'left':x,'top':y})
+    
     })
-
-// cursor 가 em 안에 들어갔을떄 cursor에 보더색이 바뀌어라
-
-
-        $('.main p em').mouseenter(function(){
-        
-        $('.cursor').addClass('style1');
-
-
-
+    
+    // cursor 가 em 안에 들어갔을때 cursor에 보더색이 바뀌어라. 
+    
+    
+    $('.main p em').mouseenter(function(){
+    
+        $('.cursor').addClass('style1')
     })
-
+    
     $('.main p em').mouseleave(function(){
-        
-        $('.cursor').remveClass('style1');
-
-
-
+    
+        $('.cursor').removeClass('style1')
     })
-
+    
+    
+    
+    
+    
     $('.main p span').mouseenter(function(){
-        
-        $('.cursor').addClass('style2');
-
-
-
+    
+        $('.cursor').addClass('style2')
     })
-
+    
     $('.main p span').mouseleave(function(){
-        
-        $('.cursor').remveClass('style2');
-
-
-
+    
+        $('.cursor').removeClass('style2')
     })
-
-
-
-
-})
+    
+    
+    })
